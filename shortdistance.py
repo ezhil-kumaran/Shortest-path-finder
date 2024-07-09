8e152dd034048d6b436c2ebe95933b1b27f70ab5
ezhil-kumaran <ezhilkumaran.l2023@vitstudent.ac.in>
2024-07-09 19:58:06 +0530

    created a maze

--- shortdistance.py
+++ shortdistance.py
@@ -2,3 +2,12 @@ import curses
 from curses import wrapper
 import time
 import queue
+print("hello")
+maze=[
+    ["#","#","#","o","#","#"],
+    ["#"," "," "," "," "," "],
+    ["#"," "," ","#","#"," "],
+    ["#"," "," "," "," "," "],
+    ["#","#","#","x","#","#"]
+]
+
